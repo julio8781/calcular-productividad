1ef6850e9aaa77ca7772161b806b9a9252db6920
julio8781 <155683263+julio8781@users.noreply.github.com>
2024-05-22 20:51:48 +0200

    Add files via upload

--- pedidos vimifar/scrip.js
+++ pedidos vimifar/scrip.js
@@ -3,36 +3,36 @@ document.getElementById('productivityForm').addEventListener('submit', function(
     event.preventDefault();
 
     const hoursWorked = parseFloat(document.getElementById('hoursWorked').value);
-    // Obtenemos el valor del campo de minutos, si está vacío o no es un número, asignamos cero
     const minutesWorked = document.getElementById('minutesWorked').value ? parseFloat(document.getElementById('minutesWorked').value) : 0;
     const linesOfCode = parseInt(document.getElementById('linesOfCode').value);
     
-    // Convertir minutos a horas
     const totalHoursWorked = hoursWorked + (minutesWorked / 60);
 
-    // Definir las líneas por hora para cada nivel
     const linesPerHourBien = 37.5;
     const linesPerHourMedio = 28.125;
     const linesPerHourMal = 18.75;
 
-    // Calcular las líneas requeridas para cada nivel basado en el total de horas trabajadas
     const requiredLinesBien = linesPerHourBien * totalHoursWorked;
     const requiredLinesMedio = linesPerHourMedio * totalHoursWorked;
     const requiredLinesMal = linesPerHourMal * totalHoursWorked;
 
     let resultMessage;
+    let resultClass;
 
     if (linesOfCode >= requiredLinesBien) {
         resultMessage = '¡Bien! Has cumplido con la productividad requerida.';
+        resultClass = 'bien-animation bien';
     } else if (linesOfCode >= requiredLinesMedio) {
         resultMessage = 'Medio. Has cumplido parcialmente con la productividad requerida.';
+        resultClass = 'medio-animation medio';
     } else if (linesOfCode >= requiredLinesMal) {
-        resultMessage = 'Mal. Necesitas más líneas  para cumplir con la productividad.';
+        resultMessage = 'Mal. Necesitas más líneas de código para cumplir con la productividad.';
+        resultClass = 'mal-animation mal';
     } else {
-        resultMessage = 'Muy Mal. Tu productividad es insuficiente te van a despedir.';
+        resultMessage = 'Muy Mal. Tu productividad es insuficiente.';
+        resultClass = 'muy-mal-animation muy-mal';
     }
-
-    document.getElementById('result').innerText = resultMessage;
+    
+    const resultElement = document.getElementById('result');
+    resultElement.innerHTML = `<span class="result-message ${resultClass}">${resultMessage}</span>`;
 });
-
-
